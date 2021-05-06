ccddc6175e6bdaae9a78c0ae19f064b8643c9804
Bipin Maharjan <21078512+iambpn@users.noreply.github.com>
2021-05-06 12:00:39 +0545

    new model added

--- ImgCaption/AI_Model/conf.py
+++ ImgCaption/AI_Model/conf.py
@@ -2,6 +2,6 @@ import os
 
 # Basic Model Configuration
 max_length_of_caption = 35
-captionModelPath = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Model", "bi_model.57.h5")
-word_to_idx_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Tokenization", "word_to_idx.pkl")
-idx_to_word_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Tokenization", "idx_to_word.pkl")
+captionModelPath = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Model", "Final_LSTM_59.5_par.h5")
+word_to_idx_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Tokenization", "word_to_idx_35_59.5.pkl")
+idx_to_word_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Tokenization", "idx_to_word_35_59.5.pkl")
